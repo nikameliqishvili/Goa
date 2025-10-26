664b60626a8ac0aa005ddc49e1dea7b6bd7ccd20
nikameliqishvili <nikameliqishvili7777@gmail.com>
2025-10-26 19:45:43 +0400

    level1 classwork

---
+++ Day 01/classwork/saklaso1.py
@@ -0,0 +1,13 @@
+#davaleba1
+name="nikameliqishvili"
+print(name)
+#davaleba2
+mama="temurimeliqishvili"
+deda="tatiana medianiki"
+print(mama)
+print(deda)
+#davaleba3
+name="nikameliqishvili"
+weli=12
+print(name)
+print(weli)
