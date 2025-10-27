6c9c0c3106181983e9df393a8a7a99e9c87a3fa9
nikameliqishvili <nikameliqishvili7777@gmail.com>
2025-10-27 21:17:19 +0400

    level1 homework

---
+++ Day 01/homework/davaleba1.py
@@ -0,0 +1,27 @@
+#davaleba1
+print("0")
+print("00")
+print("000")
+print("0000")
+print("00000")
+print("000000")
+print("0000000")
+#davaleba2
+print("xinkali")
+print("saurma")
+print("xawapuri")
+print("mxlovana")
+print("ostri")
+#davaleba3
+print("gamarjoba nika")
+#davaleba4
+print("ooooooooooooo")
+print("o gamarjoba o")
+print("ooooooooooooo")
+#davaleba5 
+print("       meniu")
+print("1. xinkali-2 lari")
+print("2. saurma-12 lari")
+print("3. xawapuri-16 lari")
+print("4. mxlovna-14 lari")
+print("5. ostri-9 lari")
