cb6ccd923df77f374bd0a990bc70b2a0348bf644
nikameliqishvili <nikameliqishvili7777@gmail.com>
2025-11-06 16:44:43 +0400

    level5

---
+++ Day05/homework/davaleba.py
@@ -0,0 +1,14 @@
+#1) დაწერეთ კოდი, სადაც მომხმარებელს მოთხოვთ ინფორმაციას მათ შესახებ: სახელი, გვარი, ასაკი, შემდეგ ეს ინფორმაცია გამოიტანეთ როგორც რამე დოკუმენტი, მაგალითად პასპორტი (თითქოს მთავრობის პროგრამაა)
+name=input("What is your name")
+surname=input("What is your surname")
+age=input("How old are you")
+year=input("WHat year were you born")
+print("-----------------------")
+print("შნი სახელია" ,name)
+print("შენი გვარია" ,surname)
+print("შენი ასაკია" ,age )
+print("შენი დაბადების წელი" ,year)
+print("-----------------------")
+
+
+
