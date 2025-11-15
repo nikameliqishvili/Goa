c6451c69e50869ed3dcc74569144f36b4903a369
nikameliqishvili <nikameliqishvili7777@gmail.com>
2025-11-15 21:01:20 +0400

    level 6 7

---
+++ Day 06/homework/hw2.py
@@ -0,0 +1,173 @@
+#1)შექმენი 5 ცვლადი სადაც შეინახავ integer ტიპის მნიშვნელობებს
+age = 12    
+year = 2013
+price = 15
+score = 70
+temperature=-12
+#2)შეინახე 5 ცვლადი სადაც შეინახავ string ტიპის მნიშვნელობებს
+name ="nika"
+city ="fasanauri"
+color = "Blue"
+movie ="zombye"
+program ="python"
+#3) შექმენი 5 folat ცვლადი
+height = 1.64
+width = 2.5
+clas= 7.590907
+price = 1500.56
+style = 777.90
+#4) შექმენი 5 ცვლადი სადაც შეინახავ boolean ტიპის მნიშვნელობე 
+number1 = False
+number2 = True
+number3 = False
+number4 = True
+number5 = True
+#5) მოახდინე სხვადასხვა რიცხვებზე მომატება(+) 10 ჯერ
+# საწყისი რიცხვები
+print(45+67)   
+#6) მოახდინე სხვადასხვა რიცხვებზე გამოკლება(-) 10 ჯერ
+a = 50
+b = 30
+c = 100
+d = 25
+e = 60
+f = 80
+g = 15
+h = 45
+i = 90
+j = 10
+
+print(a - 5)
+print(b - 2)
+print(c - 50)
+print(d - 10)
+print(e - 20)
+print(f - 15)
+print(g - 5)
+print(h - 7)
+print(i - 30)
+print(j - 3)
+print(5*3) #15
+print(2*7) #14
+print(7*4) #28
+print(8*2) #16
+print(3*2) #5
+print(6*2) #12
+print(9*2) #18
+print(20*2) #40
+print(8*3) #24
+print(30*2) #60
+#1)          (and)             
+True and True   # es aris False   
+True and False  #es aris False
+False and True  #es aris Flase
+False and False #es aris Flase      
+#2)   (or)
+True or True  #es aris True       
+True or False #es aris True 
+False or True #es aris True
+False or False #es aris Flase
+
+
+
+
+
+#8) მოახდინე სხვადასხვა რიცხვებზე გაყოფა(/) 10 ჯერ
+print(100 // 10)  # 10
+print(250 // 10)  # 25
+print(500 // 10)  # 50
+print(1200 // 10) # 120
+print(145 // 10)  # 14
+print(987 // 10)  # 98
+print(32 // 10)   # 3
+print(864 // 10)  # 86
+print(52 // 10)   # 5
+print(1000 // 10) # 100
+#10)გაიხსენე ნაშთიანი გაყოფა და მოახდინე სხვადასხვა რიცხვებზე გაყოფა(%) 10 ჯერ
+print(100 % 10)  # 0
+print(250 % 10)  # 0
+print(500 % 10)  # 0
+print(1200 % 10) # 0
+print(145 % 10)  # 5
+print(987 % 10)  # 7
+print(32 % 10)   # 2
+print(864 % 10)  # 4
+print(52 % 10)   # 2
+print(1000 % 10) # 0
+name = input("შემოიყვანე შენი სახელი: ")
+age = input("შემოიყვანე შენი ასაკი: ")
+city = input("შემოიყვანე შენი ქალაქი: ")
+hobby = input("რის კეთება გიყვარს")
+food = input("რა არის შენი საყვარელი საჭმელი: ")
+#12)შექმენი 10 ცვლადი print ფუნქციის გამოყენებით დაპრინტე ეს ცვლადები
+a = 10
+b = 20
+c = 30
+d = 40
+e = 50
+f = 60
+g = 70
+h = 80
+i = 90
+j = 100
+print(a, b, c, d, e, f, g, h, i, j)
+# print ფუნქციის შიგნით რამოდენიმე მნიშვნელობა დაბეჭდე ერთროულად. მსგავსი კოდი გაიმეორე 10 ჯერ სხვადასხვა მნიშვნელობებით
+
+print("Hello", "World", 42, 3.14, True)
+print(10, 20, 30, "Python", "Programming")
+print("Cat", "Dog", "Bird", 100, 200)
+print(1, 2, 3, "Apple", "Banana")
+print("Red", "Blue", "Green", 300, 400)
+print(50, 60, 70, "Summer", "Winter")
+print("Pizza", "Pasta", "Burger", 101, 202)
+print("Sun", "Moon", "Stars", 500, 600)
+print(12, 13, 14, "Math", "Science")
+print("Car", "Bike", "Bus", 700, 800)
+
+# დანაყოფი
+print(15 / 3)  # 5.0
+print(20 / 4)  # 5.0
+print(100 / 20)  # 5.0
+
+# მთელრიცხვითი გაყოფა
+print(15 // 10)  # 1
+print(10 // 7)  # 1
+print(40 // 12)  # 3
+
+# გამრავლება
+print(4 * 3)  # 12
+print(40 * 3)  # 120
+print(120 * 3)  # 360
+
+# ახარისხება
+print(4 ** 3)  # 64
+print(10 ** 3)  # 1000
+print(2 ** 6)  # 64
+print(3 ** 4)  # 81
+
+# ნაშთი
+print(10 % 7)  # 3
+print(3 % 2)  # 1
+print(50 % 25)  # 0
+print(14 % 11)  # 3
+print(110 % 50)  # 10
+ 
+#1)მომხმარებელს შემოატანიე რიცხვი და გააინტეჯერე იგი.
+#შეამოწმე ეს რიცხვი მეტია 100-ზე თუ არა.
+number = int (input("შემოიყვანე რიცხვი"))
+if number > 100:
+    print("ricxvi metia 100 ze")
+else:
+    print("რიცხვი ნაკლებია 100 ზე")
+# მომხმარებელს შემოაყვანინე სიტყვა. შეამოწმე შეყვანილი სიტყვა უდრის "hello"-ს თუ არ
+saxli = input("შემოიყვანე სიტყვა")
+if saxli == "hello": 
+    print("სიტყვა უდრის hello")
+else:
+    print("სიტყვა არუდრის hello")
+#შეამოწმე ეს რიცხვი დადებითია თუ უარყოფითი.
+ricxvi = int(input("შეამოიყვანე რიცხვი"))
+if ricxvi < 0:
+    print("რიცხვი არის უარყოფითი")
+else:
+    print("რიცხვი არის დადებითი")
